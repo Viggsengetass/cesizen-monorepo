9e7e6d83b3b6754dd7d074124230175f96643d33
Viggsengetass <paulito.antoine@gmail.com>
2025-06-30 15:25:59 +0200

    feat: chargement des fixtures User et Content fonctionnel

--- apps/api/migrations/Version20250630131355.php
+++ apps/api/migrations/Version20250630131355.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250625141902 extends AbstractMigration
+final class Version20250630131355 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,10 +24,16 @@ final class Version20250625141902 extends AbstractMigration
             CREATE SEQUENCE app_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE SEQUENCE content_id_seq INCREMENT BY 1 MINVALUE 1 START 1
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE app_user (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
             CREATE UNIQUE INDEX UNIQ_88BDF3E9E7927C74 ON app_user (email)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE content (id INT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, type VARCHAR(255) DEFAULT NULL, body TEXT NOT NULL, created_at VARCHAR(255) NOT NULL, updated_at VARCHAR(255) DEFAULT NULL, cover_image VARCHAR(255) DEFAULT NULL, video_url VARCHAR(255) DEFAULT NULL, media_urls VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))
         SQL);
     }
 
@@ -41,7 +47,13 @@ final class Version20250625141902 extends AbstractMigration
             DROP SEQUENCE app_user_id_seq CASCADE
         SQL);
         $this->addSql(<<<'SQL'
+            DROP SEQUENCE content_id_seq CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE app_user
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE content
         SQL);
     }
 }
---
+++ apps/api/src/Entity/Content.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ContentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContentRepository::class)]
+#[ApiResource]
+class Content
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $coverImage = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $videoUrl = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $mediaUrls = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->coverImage;
+    }
+
+    public function setCoverImage(?string $coverImage): static
+    {
+        $this->coverImage = $coverImage;
+
+        return $this;
+    }
+
+    public function getVideoUrl(): ?string
+    {
+        return $this->videoUrl;
+    }
+
+    public function setVideoUrl(?string $videoUrl): static
+    {
+        $this->videoUrl = $videoUrl;
+
+        return $this;
+    }
+
+    public function getMediaUrls(): ?string
+    {
+        return $this->mediaUrls;
+    }
+
+    public function setMediaUrls(?string $mediaUrls): static
+    {
+        $this->mediaUrls = $mediaUrls;
+
+        return $this;
+    }
+}
